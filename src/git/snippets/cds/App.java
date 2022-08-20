5a3da88a4a7e548f9c8133c301ee650e4b9c8dbe
GreyZeng <410486047@qq.com>
2022-08-20 13:41:11 +0800

    init

---
+++ src/git/snippets/cds/App.java
@@ -0,0 +1,7 @@
+package git.snippets.cds;
+
+public class App {
+    public static void main(String[] args) {
+        new Helper().greet();
+    }
+}
---
+++ src/git/snippets/cds/Helper.java
@@ -0,0 +1,7 @@
+package git.snippets.cds;
+
+public class Helper {
+    public void greet() {
+        System.out.println("Hello world!");
+    }
+}
